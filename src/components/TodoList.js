2b1b00ba6695b75262cb31d0527c152365fb8104
Aviraj Gite <giteaviraj155@gmail.com>
2026-05-06 14:19:53 +0530

    Revert "update all files"
    This reverts commit 2150289edd7b6b39c3c65888f7783227dbfc840c.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,35 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 
-const TodoList = () => {
-  const [tasks, setTasks] = useState([
-    { id: 1, task: "Learn React", isCompleted: false },
-    { id: 2, task: "Build a React app", isCompleted: false },
-    { id: 3, task: "Deploy the React App", isCompleted: false },
-  ]);
-
-  const handleComplete = (id) => {
-    setTasks((prev) =>
-      prev.map((item) =>
-        item.id === id ? { ...item, isCompleted: true } : item
-      )
+const TodoList = ({ tasks, setTasks }) => {
+  const ChangeState = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            isCompleted: true,
+          };
+        } else {
+          return item;
+        }
+      })
     );
   };
 
   return (
     <div>
       <h1>Child Component</h1>
-      <ul>
-        {tasks.map((item) => (
-          <li key={item.id}>
-            {item.task}
-            {!item.isCompleted && (
-              <button onClick={() => handleComplete(item.id)}>Complete</button>
-            )}
-          </li>
-        ))}
-      </ul>
+      {tasks.map((item) => {
+        return (
+          <div
+            key={item.id}
+            style={{ display: "flex", gap: "10px", margin: "10px" }}
+          >
+            <ul>
+              <li>
+                {item.task}
+                {!item.isCompleted && (
+                  <button onClick={() => ChangeState(item.id)}>Completed</button>
+                )}
+              </li>
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
+
 
 export default TodoList;
